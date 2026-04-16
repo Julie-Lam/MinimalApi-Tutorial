247d5aeffa21baa2de0da7328e77372ed328516a
Julie <lam.julie@hotmail.com>
2026-04-16 22:48:22 +1000

    added documentation and a .sln

--- MinimalApi.WebApi/Program.cs
+++ MinimalApi.WebApi/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+/* 
+In this section we can expose our API endpoints, with MapGet, MapPost, MapPut etc 
+*/
+
+// MapGet takes 2 parameters. 1st is the pattern, i.e. "/" specifies the path for this 'API' call. Because it is just /, it means when the site root is visited it will return this call. I.e. http://localhost:5047
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
